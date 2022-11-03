363617321aaf8b7b6434cb427698bd61339af1a1
Jason Romano <aracnid@gmail.com>
2022-11-02 23:17:05 -0400

    Set single version source

--- squaredown/__init__.py
+++ squaredown/__init__.py
@@ -1,6 +1,6 @@
 """A set of functions to retrieve and save Square data into MongoDB.
 """
-# from aracnid_config import Config
+from importlib.metadata import version
 
 from squaredown.catalog import Catalog
 from squaredown.connector import Connector
@@ -8,4 +8,4 @@ from squaredown.locations import Locations
 from squaredown.orders import Orders
 
 
-__version__ = '1.2.1'
+__version__ = version(__package__)
