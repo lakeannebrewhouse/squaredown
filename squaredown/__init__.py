424482228cf5f931f31dba5f032222ac1a0db5e4
Jason Romano <aracnid@gmail.com>
2023-03-12 12:40:05 -0400

    Build version

--- squaredown/__init__.py
+++ squaredown/__init__.py
@@ -1,11 +1,9 @@
 """A set of functions to retrieve and save Square data into MongoDB.
 """
-from importlib.metadata import version
-
 from squaredown.catalog import Catalog
 from squaredown.connector import Connector
 from squaredown.locations import Locations
 from squaredown.orders import Orders
 
 
-__version__ = version(__package__)
+__version__ = "1.4.0"
